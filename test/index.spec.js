c5d6f8f6651be40baa3972bcea8255c4cecb9741
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-11 01:24:19 -0500

    Tests for baseline behavior

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,31 @@
+import ShallowRenderer from 'react-test-renderer/shallow';
 import expect from 'expect';
-import { provideQueryParams } from '../src';
+import React from 'react'
+import { mapProps } from '../src';
+
+function MyComponent(props) {
+  return <div>{props.foo}</div>
+}
+
+const NullMappedComponent = mapProps({})(MyComponent)
+const MappedComponent = mapProps({foo: value => 4})(MyComponent)
 
 describe('provideQueryParams', () => {
-  it.skip('should work #YOLO', () => {
-    
+  it('does not change prop when map is empty', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<NullMappedComponent foo='1' />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe(MyComponent);
+    expect(result.props.foo).toEqual(1)
+  });
+  
+  it('changes prop when map is a hash', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<MappedComponent foo='1' />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe(MyComponent);
+    expect(result.props.foo).toEqual(4)
   });
 });
